57170104a89f70349d71b9c2263ac094c4e60228
Ashish Tiwari <ashish@novo.cloud>
2022-08-13 14:33:44 +0530

    Initial push

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://capable-blancmange-a47a5a.netlify.app",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join_room", (data) => {
+    let joineeNumber = 0;
+    if (
+      !(
+        io.sockets.adapter.rooms.get(data) !== undefined &&
+        io.sockets.adapter.rooms.get(data).size > 2
+      )
+    ) {
+      socket.join(data);
+      joineeNumber = io.sockets.adapter.rooms.get(data).size;
+      socket.to(data).emit("no_of_users", {
+        joinee: socket.id,
+        color: joineeNumber == 1 ? "w" : "b",
+        roomsize: io.sockets.adapter.rooms.get(data).size,
+      });
+    }
+  });
+
+  socket.on("peice_move", (data) => {
+    socket.to(data.room).emit("move", data.fen);
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    hello: "hi!",
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on port 3000");
+});
